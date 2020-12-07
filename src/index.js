a72e294c5189cb79b60d50a24be525b5577eb5b1
FacundoOs <osores.facundo@gmail.com>
2020-12-07 01:23:37 +0100

    mern app test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+
+// import reportWebVitals from './reportWebVitals';
+
+let apiUrl;
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "";
+} else {
+  apiUrl = "http://localhost:5000/";
+}
+axios.defaults.baseURL = apiUrl;
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +24,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
